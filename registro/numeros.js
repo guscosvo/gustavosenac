b28a7191b6b08bfd61506a45593b51278b00518c
GUSTAVO COSTA <04235759027@senacrs.edu.br>
2025-09-26 12:20:09 -0300

    guscosvo

---
+++ registro/numeros.js
@@ -0,0 +1,10 @@
+function recebeNumeros(){
+    var n1 = parseInt(document.getElementById('n1').value);
+    var n2 = parseInt(document.getElementById('n2').value);
+    var soma="";
+ 
+    soma = (n1+n2);
+ 
+    alert(soma);
+}
+ 
